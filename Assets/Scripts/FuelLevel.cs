a1d41fa140bad6ff8272fa6f9a612b211f71392a
Zeskos <73202593+Zeskos@users.noreply.github.com>
2023-09-07 00:08:11 -0600

    Added Fuel Bar System

---
+++ Assets/Scripts/FuelLevel.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelLevel : MonoBehaviour
+{
+    [SerializeField] Image[] healthBars;
+    [SerializeField] GameController gameController;
+
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        float oxygenLevels = gameController.GetOxygenLevel();
+
+        for (int i = 0; i < healthBars.Length; i++)
+        {
+            int threshold = 100 - (i + 1) * 10;
+            healthBars[i].gameObject.SetActive(oxygenLevels >= threshold);
+        }
+    }
+}
