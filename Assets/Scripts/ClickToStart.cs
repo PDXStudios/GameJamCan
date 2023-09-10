e4ca705f87e36a73063f1d9cabdde30f32c9214c
Zeskos <73202593+Zeskos@users.noreply.github.com>
2023-09-10 15:46:09 -0600

    Update ClickToStart.cs

--- Assets/Scripts/ClickToStart.cs
+++ Assets/Scripts/ClickToStart.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ClickToStart : MonoBehaviour
 {
     [SerializeField] GameController gameController;
+    [SerializeField] Button clickStartButton;
     // Start is called before the first frame update
     Vector3 endpoint = new Vector3 (0,1080,0);
     bool atEnd = true;
+
+
     public void ClickStart()
     {
         atEnd = false;
@@ -23,7 +28,8 @@ public class ClickToStart : MonoBehaviour
                 atEnd = true;
                 return;
             }
-            transform.position += Vector3.Lerp(Vector3.zero, endpoint, 0.5f * Time.deltaTime);
+            transform.position += Vector3.Lerp(Vector3.zero, endpoint, 0.25f * Time.deltaTime);
+            clickStartButton.gameObject.SetActive(false);
         }
         Debug.Log(transform.position);
     }
