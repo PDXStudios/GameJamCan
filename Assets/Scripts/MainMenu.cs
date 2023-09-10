47deb6ae86d419d29d666eaf164f664c9ade0657
Triston Land <tristonland@gmail.com>
2023-09-10 00:43:38 -0600

    add mainMenu Scene & music

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    //options button didnt need a script so see button for info
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit was called From the ui menu :)");
+        Application.Quit();
+    }
+}
