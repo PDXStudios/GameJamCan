38d4abf3a3720542ad3b082a70b2339a8b680653
Triston Land <tristonland@gmail.com>
2023-09-11 00:25:08 -0600

    fix screen size for all resolutions

--- Assets/Scripts/ClickToStart.cs
+++ Assets/Scripts/ClickToStart.cs
@@ -1,4 +1,3 @@
-using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,9 +9,8 @@ public class ClickToStart : MonoBehaviour
     [SerializeField] GameController gameController;
     [SerializeField] GameObject spaceObject;
     [SerializeField] Button clickStartButton;
-    Vector3 endpoint = new Vector3 (0,1080,0);
     bool atEnd = true;
-
+    int screen = Screen.height;
 
     public void ClickStart()
     {
@@ -24,16 +22,16 @@ public class ClickToStart : MonoBehaviour
     {
         if (atEnd == false) 
         {
-            if (transform.position.y >= 1080)
+            if (transform.position.y >= Screen.height)
             {
                 atEnd = true;
-                transform.position = new Vector3(960.00f, 1080.0f, 0f);
                 return;
             }
-            transform.position += Vector3.Lerp(Vector3.zero, endpoint, 0.35f * Time.deltaTime);
-            spaceObject.transform.position += Vector3.Lerp(Vector3.zero, endpoint, 0.007f * Time.deltaTime);
+            transform.position += Vector3.Lerp(Vector3.zero, new Vector3 (0, Screen.height ,0), 0.35f * Time.deltaTime);
+            spaceObject.transform.position += Vector3.Lerp(Vector3.zero, new Vector3(0, Screen.height, 0), 0.007f * Time.deltaTime);
             // transform.position += Vector3.Lerp(Vector3.zero, endpoint, 0.25f * Time.deltaTime);
             clickStartButton.gameObject.SetActive(false);
         }
+        Debug.Log(screen);
     }
 }
